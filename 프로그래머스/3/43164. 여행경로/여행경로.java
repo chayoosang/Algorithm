afd99e7c90347e16ace9739f6fcd4d7b72aed6b6
차유상 <81149634+chayoosang@users.noreply.github.com>
2023-11-12 03:26:09 +0900

    [level 3] Title: 여행경로, Time: 11.18 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+  String[][] tickets;
+    ArrayList<String> answer;
+    boolean[] visit;
+
+
+    public void dfs(int count, String city, String path) {
+        if (count == tickets.length) {
+            answer.add(path);
+        }
+
+
+        for(int i=0; i<tickets.length; i++) {
+            if (tickets[i][0].equals(city) && !visit[i]) {
+                visit[i] = true;
+                dfs( count + 1, tickets[i][1], path + " " + tickets[i][1]);
+                visit[i] = false;
+            }
+        }
+    }
+    public String[] solution(String[][] tickets) {
+        this.tickets = tickets;
+        this.visit = new boolean[tickets.length];
+        answer = new ArrayList<>();
+
+        Arrays.fill(visit, false);
+        dfs(0, "ICN", "ICN");
+
+        Collections.sort(answer);
+
+        return answer.get(0).split(" ");
+    }
+}

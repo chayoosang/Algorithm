b45dacf3c687962cf903cc5cad488898c46c36b7
차유상 <81149634+chayoosang@users.noreply.github.com>
2024-03-22 11:56:41 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func solution(_ players:[String], _ callings:[String]) -> [String] {
+    var rank = [String: Int]()
+    var players = players
+   
+    for i in 0..<players.count {
+        rank[players[i], default: 0] = i
+    }
+
+    for calling in callings {
+        let index = rank[calling]!
+        let player = players[index-1]
+       
+        rank[calling]! -= 1
+        rank[players[index-1]]! += 1
+       
+        players.swapAt(index, index-1)
+    }
+
+    return players
+}
+
